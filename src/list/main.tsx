ad878c624b89aa45ef8e98d4c8a9af27e63d1e2e
kunst1080 <kontrapunkt1080@gmail.com>
2022-07-17 23:21:56 +0900

    お気に入り登録済みに色付け

--- src/list/main.tsx
+++ src/list/main.tsx
@@ -1,10 +1,15 @@
 const allItems = document.querySelectorAll<HTMLElement>(
-    "#search_result_img_box > li, #wishlist_work tr, .n_worklist tr, .n_worklist_item, .swiper-slide"
+    "#search_result_img_box > li, #wishlist_work tr, .n_worklist tr, .n_worklist_item"
 );
 
 // init
 allItems.forEach((e) => {
     if (e.querySelector(".btn_dl")) {
         e.classList.add("mask-purchased");
+    } else if (
+        !location.pathname.includes("/wishlist/") &&
+        e.querySelector(".btn_favorite_in")
+    ) {
+        e.classList.add("mask-favorited");
     }
 });
