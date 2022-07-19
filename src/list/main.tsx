200b56041de528e6b4d664120a5944ff25e2b577
kunst1080 <kontrapunkt1080@gmail.com>
2022-07-20 00:40:19 +0900

    色付けはonloadまで待つ

--- src/list/main.tsx
+++ src/list/main.tsx
@@ -1,15 +1,19 @@
-const allItems = document.querySelectorAll<HTMLElement>(
-    "#search_result_img_box > li, #wishlist_work tr, .n_worklist tr, .n_worklist_item"
-);
+const main = () => {
+    const allItems = document.querySelectorAll<HTMLElement>(
+        "#search_result_img_box > li, #wishlist_work tr, .n_worklist tr, .n_worklist_item"
+    );
 
-// init
-allItems.forEach((e) => {
-    if (e.querySelector(".btn_dl")) {
-        e.classList.add("mask-purchased");
-    } else if (
-        !location.pathname.includes("/wishlist/") &&
-        e.querySelector(".btn_favorite_in")
-    ) {
-        e.classList.add("mask-favorited");
-    }
-});
+    // init
+    allItems.forEach((e) => {
+        if (e.querySelector(".btn_dl")) {
+            e.classList.add("mask-purchased");
+        } else if (
+            !location.pathname.includes("/wishlist/") &&
+            e.querySelector(".btn_favorite_in")
+        ) {
+            e.classList.add("mask-favorited");
+        }
+    });
+};
+
+window.addEventListener("load", main, false);
