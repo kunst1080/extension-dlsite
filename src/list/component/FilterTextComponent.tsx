78a6a9c66ec2d56c78a009bd3d3346d8d91f0021
kunst1080 <kontrapunkt1080@gmail.com>
2022-07-23 22:54:26 +0900

    割引率によるフィルターを実装

---
+++ src/list/component/FilterTextComponent.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+
+export const FilterTextComponnet = (props: {
+    children: string;
+    onUpdate: (text: string) => void;
+}) => {
+    const id = React.useId();
+    const [text, setText] = React.useState("");
+    React.useEffect(() => {
+        props.onUpdate(text);
+    });
+    return (
+        <label htmlFor={id}>
+            {props.children}
+            <input
+                id={id}
+                type="text"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+            />
+        </label>
+    );
+};
--- src/list/main.tsx
+++ src/list/main.tsx
@@ -1,19 +1,78 @@
-const main = () => {
-    const allItems = document.querySelectorAll<HTMLElement>(
-        "#search_result_img_box > li, #wishlist_work tr, .n_worklist tr, .n_worklist_item"
-    );
+import * as React from "react";
+import * as ReactDOM from "react-dom";
 
-    // init
-    allItems.forEach((e) => {
-        if (e.querySelector(".btn_dl")) {
-            e.classList.add("mask-purchased");
-        } else if (
-            !location.pathname.includes("/wishlist/") &&
-            e.querySelector(".btn_favorite_in")
-        ) {
-            e.classList.add("mask-favorited");
-        }
-    });
+import { FilterTextComponnet } from "./component/FilterTextComponent";
+
+class Item {
+    private ref: HTMLElement;
+
+    price: number;
+    isPurchased: boolean;
+    isFavorited: boolean;
+    waribikiRate: number;
+
+    constructor(ref: HTMLElement) {
+        this.ref = ref;
+        this.price = 0;
+        this.isPurchased = ref.querySelector(".btn_dl") != null;
+        this.isFavorited = ref.querySelector(".btn_favorite_in") != null;
+        this.waribikiRate = parseInt(
+            ref.querySelector<HTMLElement>(".type_sale")?.textContent || "0"
+        );
+    }
+
+    addClass(className: string) {
+        this.ref.classList.add(className);
+    }
+
+    removeClass(className: string) {
+        this.ref.classList.remove(className);
+    }
+}
+
+const main = () => {
+    const allItems: Item[] = [];
+    document
+        .querySelectorAll<HTMLElement>(
+            "#search_result_img_box > li, #wishlist_work tr, .n_worklist tr, .n_worklist_item"
+        )
+        .forEach((e) => {
+            // init
+            const it = new Item(e);
+            if (it.isPurchased) {
+                it.addClass("mask-purchased");
+            } else if (
+                !location.pathname.includes("/wishlist/") &&
+                it.isFavorited
+            ) {
+                it.addClass("mask-favorited");
+            }
+            allItems.push(it);
+        });
+
+    const filterWaribiki = (text: string) => {
+        const className = "mask-waribiki";
+        const v = parseInt(text);
+        allItems.forEach((it) => {
+            if (v > 0 && (it.waribikiRate < v || it.isPurchased)) {
+                it.addClass(className);
+            } else {
+                it.removeClass(className);
+            }
+        });
+    };
+
+    const app = document.createElement("div");
+    app.className = "custom-filter";
+    document.querySelector("#header")?.append(app);
+    ReactDOM.render(
+        <div>
+            <FilterTextComponnet onUpdate={filterWaribiki}>
+                割引率
+            </FilterTextComponnet>
+        </div>,
+        app
+    );
 };
 
 window.addEventListener("load", main, false);
