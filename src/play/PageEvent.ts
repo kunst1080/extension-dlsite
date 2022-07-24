1e1cf41db7e9dd3ac473d08ac907f97758a8a1f6
kunst1080 <kontrapunkt1080@gmail.com>
2022-07-24 20:07:11 +0900

    DOMの構造変更に追従

--- src/play/PageEvent.ts
+++ src/play/PageEvent.ts
@@ -1,23 +1,16 @@
-const CHECK_INTERVAL = 1000;
-
 var onWorkLoadCallback: (element: Element) => void;
 
 export const onAppLoad = (callback: (element: Node) => void) => {
-  let isInitialize = true;
-  const appRoot = document.querySelector("body") as Node;
   const appObserver = new MutationObserver((mutationsList, observer) => {
-    if (mutationsList.some((m) => (m.target as Element).id === "app")) {
-      const app = mutationsList[0].target;
-      if (isInitialize) {
-        callback(app);
-      }
-      isInitialize = false;
-      setTimeout(() => {
-        startObservers();
-      }, 10);
-    }
+    const list1 = mutationsList
+      .map((m) => m.target as HTMLElement)
+      .filter((e) => e.id === "App" || e.className.includes("Library_inner__"));
+    const list2 = uniq(list1);
+    list2.forEach((app) => {
+      startObservers(app);
+    });
   });
-  appObserver.observe(appRoot, {
+  appObserver.observe(document.querySelector("body") as Node, {
     attributes: false,
     childList: true,
     subtree: true,
@@ -30,40 +23,22 @@ export const onWorkLoad = (callback: (element: Element) => void) => {
 
 const uniq = <T>(array: T[]): T[] => Array.from(new Set(array));
 
-const startObservers = () => {
-  document
-    .querySelectorAll("#app .page-content .content ol.list-work li.work")
-    .forEach((n) => onWorkLoadCallback(n));
-  const content = document.querySelector("#app .page-content") as Node;
+const startObservers = (app: HTMLElement) => {
   const contentObserver = new MutationObserver((mutationsList, observer) => {
-    const items = mutationsList
+    const list1 = mutationsList
       .filter((m) => m.type === "childList")
-      .filter((m) => !(m.target as Element).classList.contains("thumbnail"))
-      // 変更のあった <ol> 要素を抽出
-      .flatMap((m) => {
-        if (m.target.nodeName === "OL") {
-          // <li> 要素が単体で挿入されたとき
-          return m.target;
-        } else {
-          if (
-            m.addedNodes.length == 1 &&
-            (m.addedNodes[0] as HTMLElement).classList?.contains("content")
-          ) {
-            // contents 全体が書き換わったとき
-            const content = m.addedNodes[0] as HTMLElement;
-            return Array.from(content.querySelectorAll("ol.list-work"));
-          } else {
-            // 購入日別の <ol> 要素が挿入されたとき
-            return Array.from(m.addedNodes).filter((n) => n.nodeName === "OL");
-          }
-        }
-      })
-      // <ol> 要素から <li> 要素を展開
-      .flatMap((n) => Array.from(n.childNodes))
-      .filter((e) => (e as Element).classList.contains("work"));
-    uniq(items).forEach((n) => onWorkLoadCallback(n as Element));
+      .map((m) => m.target as HTMLElement)
+      .flatMap((e) =>
+        Array.from(
+          e.querySelectorAll<HTMLElement>(
+            `[class^="WorkList_list__"], [class^="MylistDetailItem_list__"]`
+          )
+        )
+      );
+    const list2 = uniq(list1);
+    list2.forEach((e) => onWorkLoadCallback(e));
   });
-  contentObserver.observe(content, {
+  contentObserver.observe(app, {
     attributes: false,
     childList: true,
     subtree: true,
--- src/play/main.tsx
+++ src/play/main.tsx
@@ -30,87 +30,99 @@ const sortMylist = (lists: Mylist[]): Mylist[] =>
 
 Promise.all([db.purchase.findAll(), db.mylist.findAll().then(sortMylist)]).then(
     ([purchases, mylists]) => {
-    const findWork = (element: Element): Purchase | null => {
-        const workName = findByQuery(element, ".work-name").innerText;
-        const makerNmaae = findByQuery(element, ".maker-name").innerText;
-        const work = purchases.find(
-            (p) => p.maker_name === makerNmaae && p.work_name === workName
-        );
-        return work as Purchase;
-    };
+        const findWork = (element: Element): Purchase | null => {
+            const workName = findByQuery(
+                element,
+                `[class*="_workName__"]`
+            ).innerText;
+            const makerName = findByQuery(
+                element,
+                `[class*="_makerName__"]`
+            ).innerText;
+            const work = purchases.find(
+                (p) => p.maker_name === makerName && p.work_name === workName
+            );
+            return work as Purchase;
+        };
 
         const workMylistMap = mylists.reduce((acc, lst) => {
             lst.worknos.forEach((no) => {
                 if (!acc.has(no)) acc.set(no, []);
                 acc.get(no)?.push(lst);
             });
-        return acc;
-    }, new Map<string, Mylist[]>());
+            return acc;
+        }, new Map<string, Mylist[]>());
 
-    const workElementMap = new Map<string, Element>();
+        const workElementMap = new Map<string, Element>();
 
-    let selectedMylistIds: string[] = [];
-    const filterElement = (e: Element, mylists: Mylist[]) => {
-        if (
-            selectedMylistIds.length == 0 ||
-            (selectedMylistIds.includes("-1") && mylists.length == 0) ||
+        let selectedMylistIds: string[] = [];
+        const filterElement = (e: Element, mylists: Mylist[]) => {
+            if (
+                selectedMylistIds.length == 0 ||
+                (selectedMylistIds.includes("-1") && mylists.length == 0) ||
                 mylists.some((l) =>
                     selectedMylistIds.includes(String(l.mylist_id))
                 )
-        ) {
-            (e as HTMLElement).style.display = "";
-        } else {
-            (e as HTMLElement).style.display = "none";
-        }
-    };
-
-    onWorkLoad((e) => {
-        if (e.attributes.getNamedItem("extension-dlsite")) return;
-        e.attributes.setNamedItem(document.createAttribute("extension-dlsite"));
-        const app = document.createElement("div");
-        e.append(app);
-        const work = findWork(e);
-        if (work) {
-            const lists = workMylistMap.get(work.workno) || [];
-            ReactDOM.render(<TagComponent mylists={sortMylist(lists)} />, app);
-            workElementMap.set(work.workno, e);
-            filterElement(e, lists);
-        } else {
-            console.error(`Purchase not found`);
-            console.error(e);
-            ReactDOM.render(<ErrorComponent />, app);
-        }
-    });
-    onAppLoad((e) => {
-        // Hide logout
-        const logoutMenu = document.querySelector(
-            "nav.slide-menu .page-content > div:last-of-type"
-        ) as HTMLElement | null;
-        if (logoutMenu) logoutMenu.hidden = true;
-        // Mylist Filter
-        const app = document.createElement("div");
-        document.querySelector("nav.slide-menu .page-content")?.append(app);
-        const onFilterChange = (values: string[]) => {
-            selectedMylistIds = values;
-            workElementMap.forEach((e, workno) => {
-                const lists = workMylistMap.get(workno) || [];
-                filterElement(e, lists);
-            });
+            ) {
+                (e as HTMLElement).style.display = "";
+            } else {
+                (e as HTMLElement).style.display = "none";
+            }
         };
-        const options = [
-            {
-                value: "-1",
-                label: "<マイリストなし>",
-            },
-        ].concat(
-            mylists.map((m) => ({
-                value: m.mylist_id,
-                label: m.mylist_name,
-            }))
-        );
-        ReactDOM.render(
-            <FilterComponent options={options} onChange={onFilterChange} />,
-            app
-        );
-    });
-});
+
+        onWorkLoad((e) => {
+            if (e.attributes.getNamedItem("extension-dlsite")) return;
+            e.attributes.setNamedItem(
+                document.createAttribute("extension-dlsite")
+            );
+            const app = document.createElement("div");
+            e.append(app);
+            const work = findWork(e);
+            if (work) {
+                const lists = workMylistMap.get(work.workno) || [];
+                ReactDOM.render(
+                    <TagComponent mylists={sortMylist(lists)} />,
+                    app
+                );
+                workElementMap.set(work.workno, e);
+                filterElement(e, lists);
+            } else {
+                console.error(`Purchase not found`);
+                console.error(e);
+                ReactDOM.render(<ErrorComponent />, app);
+            }
+        });
+        onAppLoad((e) => {
+            // Hide logout
+            const logoutMenu = document.querySelector(
+                "nav.slide-menu .page-content > div:last-of-type"
+            ) as HTMLElement | null;
+            if (logoutMenu) logoutMenu.hidden = true;
+            // Mylist Filter
+            const app = document.createElement("div");
+            document.querySelector("nav.slide-menu .page-content")?.append(app);
+            const onFilterChange = (values: string[]) => {
+                selectedMylistIds = values;
+                workElementMap.forEach((e, workno) => {
+                    const lists = workMylistMap.get(workno) || [];
+                    filterElement(e, lists);
+                });
+            };
+            const options = [
+                {
+                    value: "-1",
+                    label: "<マイリストなし>",
+                },
+            ].concat(
+                mylists.map((m) => ({
+                    value: m.mylist_id,
+                    label: m.mylist_name,
+                }))
+            );
+            ReactDOM.render(
+                <FilterComponent options={options} onChange={onFilterChange} />,
+                app
+            );
+        });
+    }
+);
